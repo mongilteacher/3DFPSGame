1d6e7a730934d77983eef45c5d4cf51136b78027
mongilteacher <mongilteacher@gmail.com>
2024-02-16 14:56:04 +0900

    연사

--- Assets/02.Scripts/Player/PlayerGunFire.cs
+++ Assets/02.Scripts/Player/PlayerGunFire.cs
@@ -8,18 +8,21 @@ public class PlayerGunFire : MonoBehaviour
     // 필요 속성
     // - 총알 튀는 이펙트 프리팹
     public ParticleSystem HitEffect;
-    // 구현 순서.
-    // 1. 만약에 마우스 왼쪽 버튼을 누르면
-    // 2. 레이(광선)을 생성하고, 위치와 방향을 설정한다.
-    // 3. 레이를 발사한다.
-    // 4. 레이가 부딛힌 대상의 정보를 받아온다.
-    // 5. 부딛힌 위치에 (총알이 튀는)이펙트를 생성한다.
+ 
+    // - 발사 쿨타임
+    public float FireCooltime = 0.2f;
+    private float _timer;
+    
 
     private void Update()
     {
-        // 1. 만약에 마우스 왼쪽 버튼을 누르면
-        if (Input.GetMouseButtonDown(0))
+        _timer += Time.deltaTime;
+        
+        // 1. 만약에 마우스 왼쪽 버튼을 누른 상태 && 쿨타임이 다 지난 상태
+        if (Input.GetMouseButton(0) && _timer >= FireCooltime)
         {
+            _timer = 0;
+            
             // 2. 레이(광선)을 생성하고, 위치와 방향을 설정한다.
             Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
             // 3. 레이를 발사한다.

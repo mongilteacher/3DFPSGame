0936c46de1342a5fbe9fde723a3238efb01baead
mongilteacher <mongilteacher@gmail.com>
2024-02-26 14:06:37 +0900

    텍스트메시프로 폰트 굽기

--- Assets/02.Scripts/Item/ItemManager.cs
+++ Assets/02.Scripts/Item/ItemManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +10,9 @@ public class ItemManager : MonoBehaviour
 {
     public static ItemManager Instance { get; private set; }
 
-    public Text HealthItemCountTextUI;
-    public Text StaminaItemCountTextUI;
-    public Text BulletItemCountTextUI;
+    public TextMeshProUGUI HealthItemCountTextUI;
+    public TextMeshProUGUI StaminaItemCountTextUI;
+    public TextMeshProUGUI BulletItemCountTextUI;
     
 
     private void Awake()

e59e9022af3e7451bf6b4b469373019d4e8e35a9
mongilteacher <mongilteacher@gmail.com>
2024-03-04 09:49:47 +0900

    게임 종료 구현

--- Assets/02.Scripts/UI/UI_OptionPopup.cs
+++ Assets/02.Scripts/UI/UI_OptionPopup.cs
@@ -40,5 +40,13 @@ public class UI_OptionPopup : MonoBehaviour
     public void OnClickExitButton()
     {
         Debug.Log("게임종료");
+
+        // 빌드 후 실행했을 경우 종료하는 방법
+        Application.Quit();
+
+#if UNITY_EDITOR
+        // 유니티 에디터에서 실행했을 경우 종료하는 방법
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif        
     }
 }
